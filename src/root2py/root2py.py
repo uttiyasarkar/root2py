fe869b8fb0fdf74b5d1bcc9183f2b4cbbc2ab2bf
uttiya <uttiyasarkar@gmail.com>
2023-01-27 15:53:53 +0100

    src: source code updated, functioning for a simple test root file

--- src/root2py/root2py.py
+++ src/root2py/root2py.py
@@ -1,12 +1,36 @@
+######Required libraries########
 import uproot
+import pickle5 as pickle
 import pandas as pd
 import awkward as ak
-def open(filename: str,tree: str,branches: list)->ak:
-    return uproot.open(filename)[tree].arrays(branches, library='ak')
-    ak_branches = ak["gen_pt"]
-def save(filename: str,branches: list,data: ak):
-    store = pd.HDFStore(filename, mode='w')
-    for i in list(branches):
-        print(branches[i])
-        store[branches[i]] = open(filename,tree,branches)
+
+
+kwargs = dict(filename=str,tree=str,branches=list)  ###dict object kwargs common between different definitions
+#print(kwargs)
+
+###############Def opens any root file by using uproot###################
+def open(**kwargs)->ak:
+    return uproot.open(kwargs['filename'])[kwargs['tree']].arrays(kwargs['branches'], library='ak')
+
+###############Def saves any root file to a hdf format##################
+def savehdf(outfile: str,**kwargs):
+    df =  open(**kwargs)
+    #
+    store = ak.to_dataframe(df)  
+        #print(df[i])
+    store.to_hdf(outfile,'key',mode='w')         
+                   # store[branches[i]] = open(filename,tree=tree,branches=branches)
+            #print(store)
+    return store
+
+pkloption = {'compression': str,'protocol': int, 'storage_option': dict}
+###############Def saves any root file to a pkl format##################
+def savepkl(outfile: str,*pkloption,**kwargs):
+    df =  open(**kwargs)
+    #
+    store = ak.to_dataframe(df)  
+        #print(df[i])
+    store.to_pickle(outfile,*pkloption)         
+                   # store[branches[i]] = open(filename,tree=tree,branches=branches)
+            #print(store)
     return store
